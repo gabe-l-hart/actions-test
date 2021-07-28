520e22d66a98f7bf1f86c66a83e705f716d36a0a
Gabe Goodhart <ghart@us.ibm.com>
2021-07-28 15:27:58 -0600

    main: Set up top-level release.sh to dispatch different release types

--- release.sh
+++ release.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
 # Get the tag for this release
-tag=$(echo $HTML_URL | rev | cut -d'/' -f1 | rev)
+tag=$(echo $REF | rev | cut -d'/' -f1 | rev)
 echo "The tag is ${tag}!!"
+
+# Parse the tag for prefix and suffix
+release_type=$(echo $tag  cut -d'-' -f1)
+version=$(echo $tag  cut -d'-' -f2)
+
+# Dispatch to the various types of releases
+if [ "$release_type" == "test" ]
+then
+    echo "Running a TEST release"
+elif [ "$release_type" == "python" ]
+then
+    cd src/python
+    docker build . \
+        --target=publish \
+        --build-arg PYTHON_RELEASE_VERSION=$version \
+        --build-arg PYPI_TOKEN="test-value"
+else
+    echo "Unknown release type: [$release_type]"
+    exit 1
+fi
