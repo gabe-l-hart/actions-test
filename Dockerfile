c645d1e7fddadd913651643082cf45dd7b60152e
Gabe Goodhart <ghart@us.ibm.com>
2021-08-06 11:25:42 -0600

    main: Testing for alog ts

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
-FROM python:3.6-slim as tests
+FROM python:3.6-slim as base
 
 RUN echo "Hello World!"
+ARG TEST_ARG
+
+FROM base as next
+ARG TEST_ARG
+RUN echo "${TEST_ARG}"
+
+FROM python:3.6-slim as restart
+ARG TEST_ARG
+RUN echo "${TEST_ARG}"
--- src/ts/alog_example.ts
+++ src/ts/alog_example.ts
@@ -2,7 +2,7 @@ import * as alog from 'alchemy-logging';
 
 const cfg: alog.AlogConfig = {
   defaultLevel: alog.DEBUG,
-  formatter: alog.JsonFormatter,
+  formatter: alog.PrettyFormatter,
 };
 alog.configure(cfg);
 alog.info('TEST', 'This is a test');
